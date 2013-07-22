ae748b13d9ea4893a8a3cb12b918d4da3f84adcf
wahabakhtar <wahab.akhtar@live.com>
2013-07-22 02:14:03 -0700

    Updated web.js

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
   fs.readFileSync('index.html', function (err, data) {
     if (err) throw err;
     response.send(data);
-  }
+  })
 });
 
 var port = process.env.PORT || 5000;
